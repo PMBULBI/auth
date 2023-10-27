b4a32585cddf8ded45cf52d68abbb9c16540ae03
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-27 16:31:25 +0700

    fix(token):deklarasi token

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { setInner } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
-token=getCookie("login");
+let token=getCookie("login");
 setInner("nama","Bambang Pamungkas "+token);
