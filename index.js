c7b360948bf6ebebda841e125bc07d3573c7fc07
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-30 07:51:09 +0700

    add(if):redirect klo kosong cookies

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ import { setInner } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 let token=getCookie("login");
-setInner("nama","Bambang Pamungkas "+token);
+if (token === ""){
+    window.location.replace("../");
+}else{
+    setInner("nama","Bambang Pamungkas "+token);
+}
+
