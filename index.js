017e6c8f58df51a600e2d035492106f77af93a16
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2023-12-01 14:21:22 +0700

    feat: check if token registered

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import { setInner } from "https://jscroot.github.io/element/croot.js";
-import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+import { getCookie, setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
-let token=getCookie("login");
+let token = getCookie("login");
 if (token === ""){
     window.location.replace("../");
 }else{
     setInner("Selamat Datang di Sistem PMB","Anda akan diarahkan ke laman selanjutnya "+token);
+    getWithHeader("https://komarbe.ulbi.ac.id/pendaftar/pendaftar/registered", "LOGIN", token, responseData)
     window.location.replace("../pmb-mhs/");
 }
 
+
+
+function responseData(result){
+    if (result.data.is_registered){
+        window.location.replace("../pmb-mhs/");
+    }else{
+        setInner("Selamat Datang di Sistem PMB","Silahkan Lakukan Pendaftaran "+token);
+        getWithHeader("https://komarbe.ulbi.ac.id/pendaftar/pendaftar/registered", "LOGIN", token, responseData);
+        setCookieWithExpireHour("no_hp", result.data.phone_num, 2);
+        window.location.replace("../signup");
+    }
+    
+}
