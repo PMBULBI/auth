4076e822b6562f7f692b0d9aab446cf472ca72d9
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2023-12-01 14:26:11 +0700

    fix: error set inner

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let token = getCookie("login");
 if (token === ""){
     window.location.replace("../");
 }else{
-    setInner("Selamat Datang di Sistem PMB","Anda akan diarahkan ke laman selanjutnya "+token);
+    setInner("nama","Anda akan diarahkan ke laman selanjutnya "+token);
     getWithHeader("https://komarbe.ulbi.ac.id/pendaftar/pendaftar/registered", "LOGIN", token, responseData)
     window.location.replace("../pmb-mhs/");
 }
@@ -17,7 +17,7 @@ function responseData(result){
     if (result.data.is_registered){
         window.location.replace("../pmb-mhs/");
     }else{
-        setInner("Selamat Datang di Sistem PMB","Silahkan Lakukan Pendaftaran "+token);
+        setInner("nama","Silahkan Lakukan Pendaftaran "+token);
         getWithHeader("https://komarbe.ulbi.ac.id/pendaftar/pendaftar/registered", "LOGIN", token, responseData);
         setCookieWithExpireHour("no_hp", result.data.phone_num, 2);
         window.location.replace("../signup");
