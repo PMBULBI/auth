58d5f671b55d5eef616163a12afe20e11498619a
JPratama7 <josepratama080@gmail.com>
2023-12-14 17:46:42 +0700

    feat+refactor: refactor admin login qr

--- index.js
+++ index.js
@@ -13,40 +13,43 @@ const main = async () =>{
     let token = getCookie("login");
     if (token === ""){
         window.location.replace("../");
-    }else{
-        postWithToken("https://komarbe.ulbi.ac.id/refresh/token","LOGIN",token,"",setcookie);
-        token = getCookie("login");
-        setInner("nama","Anda akan diarahkan ke laman selanjutnya "+token);
-        getWithHeader("https://komarbe.ulbi.ac.id/isadmin", "LOGIN", token, responseDataAdmin);
-        getWithHeader("https://komarbe.ulbi.ac.id/pendaftar/pendaftar/registered", "LOGIN", token, responseData);
-        // window.location.replace("../pmb-mhs/");
+        return;
     }
+
+    postWithToken("https://komarbe.ulbi.ac.id/refresh/token", "LOGIN", token, "", setcookie);
+    token = await getCookie("login");
+    setInner("nama","Anda akan diarahkan ke laman selanjutnya "+token);
+    getWithHeader("https://komarbe.ulbi.ac.id/isadmin", "LOGIN", token, responseDataAdmin);
+    getWithHeader("https://komarbe.ulbi.ac.id/pendaftar/pendaftar/registered", "LOGIN", token, responseData);
+    window.location.replace("../pmb-mhs/");
+    return;
 
 }
 
 function responseDataAdmin(result) {
     setCookieWithExpireHour("admin status", result.success);
-
     if (result.success) {
-        // setCookieWithExpireHour("Masuk ke Admin", "ok");
-        // window.location.replace("../pmb-admin/");
-    } else {
-        setCookieWithExpireHour("Masuk ke Bukan Admin", "Bukan Admin");
-        // getWithHeader("https://komarbe.ulbi.ac.id/pendaftar/pendaftar/registered", "LOGIN", token, responseData);
+        setCookieWithExpireHour("Masuk ke Admin", "ok");
+        window.location.replace("../pmb-admin/");
+        return;
     }
+
+    setCookieWithExpireHour("Masuk ke Bukan Admin", "Bukan Admin");
+    return;
 }
 
 function setcookie(res){
-    setCookieWithExpireHour("login", result.token, 18);
+    setCookieWithExpireHour("login", res.data.token, 18);
+    return;
 }
 
 function responseData(result){
     if (result.data.is_registered){
-        // window.location.replace("../pmb-mhs/");
+        window.location.replace("../pmb-mhs/");
     } else {
         setInner("nama", "Silahkan Lakukan Pendaftaran " + token);
         setCookieWithExpireHour("no_hp", result.data.phone_num, 2);
-        // window.location.replace("../signup");
+        window.location.replace("../signup");
     }
 }
 
